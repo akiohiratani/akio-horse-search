a69c76b39763763b5fbe264470024beddfbd4e60
LAPTOP-7FAJG6I2\81909 <axxxhiratani@gmial.com>
2025-04-23 21:23:58 +0900

    馬のドメインを変更

--- src/app/domain/models/Horse.ts
+++ src/app/domain/models/Horse.ts
@@ -2,15 +2,10 @@
 export interface Horse {
     id: string;          // 馬ID
     name: string;        // 馬名
-    gender: '牡' | '牝' | 'セン'; // 性別
-    sire: string;        // 父馬名
-    dam: string;         // 母馬名
-    damSire: string;     // 母父馬名
-    owner: string;       // 馬主
-    breeder: string;     // 生産者
-    totalPrizeMoney: number; // 総賞金（円）
-    trainer?: string;    // 調教師（任意）
-    birthyear?: string;  // 生産年（任意）
+    sex: string; // 性別
     image: string;       // 馬の画像URL
+    father: string;        // 父馬名
+    grandfather: string;     // 母父馬名
+    title: string;
     detailUrl: string;   // Netkeibaの馬のURL
 }
