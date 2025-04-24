e67b6bf7191b721cc0fdaa5c01b568d5d97a9bb6
LAPTOP-7FAJG6I2\81909 <axxxhiratani@gmial.com>
2025-04-24 20:29:28 +0900

    取得するレースのドメイン定義

---
+++ src/app/domain/models/Race.ts
@@ -0,0 +1,8 @@
+// レースの型定義
+export interface Race {
+    id: string;          // レースのID
+    name: string;        // レース名
+    place: string;       // 開催場所
+    date: string;        // 開催日
+    distance: string;    // 距離
+}
